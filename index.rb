b96927c31f8270948b25380c2e1ed4138f82da08
tanakayuki <a0673040@gmail.com>
2013-07-05 20:37:21 +0900

    add tweet

--- index.rb
+++ index.rb
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
 require 'forgery_ja'
 
+sentence = []
+open('./dict/sentence.txt') {|file|
+  while l = file.gets
+    sentence.push(l.chomp)
+  end
+}
+
+len = sentence.length
+
 loop do
- p  "名前:" + ForgeryJa(:name).full_name + "|" +
-    "住所:" + ForgeryJa(:address).full_address + "|" +
-    "所持金:" + ForgeryJa(:monetary).formatted_money
+ p "名前:" + ForgeryJa(:name).full_name +
+   "("     + ForgeryJa(:name).full_name(:to => ForgeryJa::HIRA) + ") |" +
+   "住所:" + ForgeryJa(:address).full_address + "|" +
+   "所持金:" + ForgeryJa(:monetary).formatted_money + "|" +
+   "tweet:" + sentence[rand(len - 1)]
 end
